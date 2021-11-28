a471900bce616fb1bc4c98b48d28a9a76f080bf3
Harsh Saxena <harshsaxena400@gmail.com>
2021-11-28 14:36:56 +0530

    Initial commit

---
+++ app/src/main/java/com/babasxn/wazirassist/MainActivity.java
@@ -0,0 +1,14 @@
+package com.babasxn.wazirassist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
